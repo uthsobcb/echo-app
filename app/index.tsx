38b568a11b23aa3d7ec15d7c3315139e23cac1e5
Uthsob Chakraborty <uthsob9@gmail.com>
2026-01-02 15:33:55 +0600

    fix: Update authentication check to redirect unauthenticated users to the signin page

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import { Redirect } from "expo-router";
 
 export default function Index() {
-  const isAuthenticated = true;
+  const isAuthenticated = false;
 
   if (!isAuthenticated) {
     return <Redirect href="/(auth)/signin" />;
