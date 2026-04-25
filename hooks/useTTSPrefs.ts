73374445fe3ee1e0a9e8765711fd794f1eef611d
Uthsob Chakraborty <uthsob9@gmail.com>
2026-04-26 01:17:52 +0600

    feat: add useTTSPrefs hook for shared TTS enabled/rate state

---
+++ hooks/useTTSPrefs.ts
@@ -0,0 +1,33 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
+
+export const TTS_ENABLED_KEY = 'echo_tts_enabled';
+export const TTS_RATE_KEY = 'echo_tts_rate';
+
+export type TTSRate = 0.8 | 1.0 | 1.3;
+
+export function useTTSPrefs() {
+  const [enabled, setEnabledState] = useState(false);
+  const [rate, setRateState] = useState<TTSRate>(1.0);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    AsyncStorage.multiGet([TTS_ENABLED_KEY, TTS_RATE_KEY]).then(([[, en], [, rt]]) => {
+      if (en !== null) setEnabledState(en === 'true');
+      if (rt !== null) setRateState(parseFloat(rt) as TTSRate);
+      setLoaded(true);
+    });
+  }, []);
+
+  const setEnabled = async (val: boolean) => {
+    setEnabledState(val);
+    await AsyncStorage.setItem(TTS_ENABLED_KEY, val.toString());
+  };
+
+  const setRate = async (val: TTSRate) => {
+    setRateState(val);
+    await AsyncStorage.setItem(TTS_RATE_KEY, val.toString());
+  };
+
+  return { enabled, rate, setEnabled, setRate, loaded };
+}
