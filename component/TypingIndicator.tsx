232240d8719d215a30fc252839a1c700f272ff37
Uthsob Chakraborty <uthsob9@gmail.com>
2026-04-26 01:24:37 +0600

    feat: replace dot typing indicator with EchoAvatar thinking expression

--- component/TypingIndicator.tsx
+++ component/TypingIndicator.tsx
@@ -1,75 +1,49 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { View } from 'react-native';
 import Animated, {
-    useSharedValue,
-    useAnimatedStyle,
-    withRepeat,
-    withSequence,
-    withTiming,
-    withDelay,
-    Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withSequence,
+  withTiming,
 } from 'react-native-reanimated';
 import { ThemeColors } from '../context/ThemeContext';
+import { EchoAvatar } from './EchoAvatar';
 
 interface TypingIndicatorProps {
-    colors: ThemeColors;
+  colors: ThemeColors;
 }
 
-const Dot = ({ delay, colors }: { delay: number; colors: ThemeColors }) => {
-    const translateY = useSharedValue(0);
-
-    useEffect(() => {
-        translateY.value = withDelay(
-            delay,
-            withRepeat(
-                withSequence(
-                    withTiming(-6, { duration: 300, easing: Easing.out(Easing.ease) }),
-                    withTiming(0, { duration: 300, easing: Easing.in(Easing.ease) }),
-                ),
-                -1,
-                false,
-            ),
-        );
-    }, []);
-
-    const animatedStyle = useAnimatedStyle(() => ({
-        transform: [{ translateY: translateY.value }],
-    }));
-
-    return (
-        <Animated.View
-            style={[
-                {
-                    width: 7,
-                    height: 7,
-                    borderRadius: 3.5,
-                    backgroundColor: colors.textSecondary,
-                    marginHorizontal: 2,
-                },
-                animatedStyle,
-            ]}
-        />
-    );
-};
-
 export const TypingIndicator: React.FC<TypingIndicatorProps> = ({ colors }) => {
-    return (
-        <View
-            style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                paddingHorizontal: 16,
-                paddingVertical: 12,
-                borderRadius: 18,
-                borderBottomLeftRadius: 4,
-                backgroundColor: colors.surfaceSecondary,
-                alignSelf: 'flex-start',
-                marginBottom: 12,
-            }}
-        >
-            <Dot delay={0} colors={colors} />
-            <Dot delay={150} colors={colors} />
-            <Dot delay={300} colors={colors} />
-        </View>
+  const bounceY = useSharedValue(0);
+
+  React.useEffect(() => {
+    bounceY.value = withRepeat(
+      withSequence(
+        withTiming(-5, { duration: 400 }),
+        withTiming(0,  { duration: 400 }),
+      ),
+      -1,
+      true,
     );
+  }, []);
+
+  const bounceStyle = useAnimatedStyle(() => ({
+    transform: [{ translateY: bounceY.value }],
+  }));
+
+  return (
+    <View
+      style={{
+        paddingHorizontal: 12,
+        paddingVertical: 8,
+        alignSelf: 'flex-start',
+        marginBottom: 12,
+      }}
+    >
+      <Animated.View style={bounceStyle}>
+        <EchoAvatar expression="thinking" size={48} animated={false} speaking />
+      </Animated.View>
+    </View>
+  );
 };
